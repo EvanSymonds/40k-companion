a23bfda2e924e685963a932150fdf676d6283271
EvanSymonds <evan.symonds@gmail.com>
2019-02-02 20:54:55 +0000

    successful connection between front and back end

--- 40kexpress/routes/armyBuilder.js
+++ 40kexpress/routes/armyBuilder.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const debug = require('debug')('app:debug');
 
 router.get('/', (req,res) => {
   res.send('Army builder page');
+})
+
+router.put('/', (req,res) => {
+  debug('Connected');
 })
 
 module.exports = router;
